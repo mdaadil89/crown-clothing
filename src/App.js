8e0c7fbc2b0932799b8d24933d4c318fbaddfb2c
Md Aadil <mdaadilhus9@gmail.com>
2020-06-14 01:22:43 +0530

    Adding routing with react-router, then creating new routes for our existing homepage and our new hats page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
-
-import HomePage from './pages/homepage.component';
+import { BrowserRouter as Router, Switch,Route} from 'react-router-dom';
+ 
+import HomePage from './pages/homepage/homepage.component';
 
 import './App.css';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   
     return (
       <div >
-        <HomePage />
+        <Router>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route  path='/' component={HatsPage} />
+        </Switch>
+        </Router>
       </div>
     );
   
