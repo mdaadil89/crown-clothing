866c7bce87cf8bc7fc99af64855f774db49e6f83
Md Aadil <mdaadilhus9@gmail.com>
2020-06-15 00:34:07 +0530

    Adding Header Component containing link to Shop page  along with Logo containg link to /

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {  Switch,Route} from 'react-router-dom';
  
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 
 import './App.css';
 
@@ -12,6 +13,7 @@ function App() {
   
     return (
       <div >
+        <Header/>
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route  path='/shop' component={ShopPage} />
