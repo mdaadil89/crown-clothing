fb79805b1a10c8ae8af6f9b43cbc48719b9be9b6
Md Aadil <mdaadilhus9@gmail.com>
2020-06-15 00:12:21 +0530

    created collection-preview and collection-item components for the shop page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch,Route} from 'react-router-dom';
+import {  Switch,Route} from 'react-router-dom';
  
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx';
 
 import './App.css';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+
 
 function App() {
   
     return (
       <div >
-        <Router>
         <Switch>
           <Route exact path='/' component={HomePage} />
-          <Route  path='/' component={HatsPage} />
+          <Route  path='/shop' component={ShopPage} />
         </Switch>
-        </Router>
       </div>
     );
   
